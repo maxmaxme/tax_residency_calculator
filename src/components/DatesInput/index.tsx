4721cbc0250970313481457ac47bc2b2f06ecc9b
Maxim Lepekha <m.lepekha@yandex.ru>
2022-03-19 09:45:07 +0300

    dates input

---
+++ src/components/DatesInput/index.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Interval } from '../../types/interval';
+
+type Props = {
+  intervals: Interval[],
+  setIntervals(intervals: Interval[]): void,
+}
+
+type IntervalInputProps = {
+  start?: Date,
+  end?: Date,
+  onChange(start?: Date, end?: Date): void,
+  removeInterval(): void,
+}
+
+const IntervalInput = ({
+  start,
+  end,
+  onChange,
+  removeInterval,
+}: IntervalInputProps) => {
+  const toInput = (date?: Date) => {
+    if (!date) return '';
+
+    return date.getFullYear().toString().padStart(4, '0') +
+      '-' +
+      (date.getMonth() + 1).toString().padStart(2, '0') +
+      '-' +
+      date.getDate().toString().padStart(2, '0');
+  };
+
+  const onStartChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const start = new Date(e.target.value);
+    onChange(start, end);
+  };
+
+  const onEndChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const end = new Date(e.target.value);
+    onChange(start, end);
+  };
+
+  return (
+    <div>
+      <input type="date" value={toInput(start)} onChange={onStartChange} />
+      <input type="date" value={toInput(end)} onChange={onEndChange} />
+      <button onClick={removeInterval}>×</button>
+    </div>
+  );
+};
+export const DatesInput = ({
+  intervals,
+  setIntervals,
+}: Props) => {
+  const todayTimestamp = new Date().getTime();
+
+  const onChange = (index: number) => (start?: Date, end?: Date) => {
+    const newIntervals = [...intervals];
+    if (!start) return;
+    newIntervals[index] = [start?.getTime(), end?.getTime() || todayTimestamp];
+    setIntervals(newIntervals);
+  };
+
+  const removeInterval = (index: number) => () => {
+    const newIntervals = [...intervals];
+    newIntervals.splice(index, 1);
+    setIntervals(newIntervals);
+  };
+  return (<>
+    {intervals.map(([start, end], index) => {
+      return (
+        <IntervalInput
+          key={index}
+          start={new Date(start)}
+          end={new Date(end)}
+          onChange={onChange(index)}
+          removeInterval={removeInterval(index)}
+        />
+      );
+    })}
+    <button onClick={() => setIntervals([...intervals, [todayTimestamp, todayTimestamp]])}>+</button>
+  </>);
+};
